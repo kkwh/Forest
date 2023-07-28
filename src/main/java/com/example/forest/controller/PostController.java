a1108b0438f89578793178d74f75760dfd486d90
kkwh <kwh5005@naver.com>
2023-07-28 16:56:46 +0900

    [김우현] Feat: 게시물 basement

--- src/main/java/com/example/forest/controller/PostController.java
+++ src/main/java/com/example/forest/controller/PostController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.forest.dto.post.PostCreateDto;
 import com.example.forest.model.Post;
 import com.example.forest.service.PostService;
 
@@ -39,6 +41,29 @@ public class PostController {
     @GetMapping("/create")
     public void create() {
         log.info("create() GET");
+    }
+    
+    @PostMapping("/create")
+    public String createPost(PostCreateDto dto) {
+        log.info("create(dto={}) POST", dto);
+        
+        // form에서 submit(제출)된 내용을 DB 테이블에 insert
+       postService.create(dto);
+        
+        // DB 테이블 insert 후 포스트 목록 페이지로 redirect 이동.
+        return "redirect:/post";
+    }
+    
+    @GetMapping("/details")
+    public void read(Long id, Model model) {
+        log.info("read(id={})", id);
+        
+        // id로 Posts 테이블에서 id에 해당하는 포스트를 검색.
+        Post post = postService.read(id);
+               
+        // 결과를 model에 저장 -> 뷰로 전달됨.   
+        model.addAttribute("post", post);
+
     }
     
 
--- src/main/java/com/example/forest/service/PostService.java
+++ src/main/java/com/example/forest/service/PostService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.forest.dto.post.PostCreateDto;
 import com.example.forest.model.Post;
 import com.example.forest.repository.PostRepository;
 
@@ -26,26 +27,26 @@ public class PostService {
         return postRepository.findByOrderByIdDesc();
     }
     
-//    // DB POSTS 테이블에 엔터티를 삽입(insert):
-//    public Post create(PostCreateDto dto) {
-//        log.info("create(dto={})", dto);
-//        
-//        // DTO를 Entity로 변환:
-//        Post entity = dto.toEntity();
-//        log.info("entity={}", entity);
-//        
-//        postRepository.save(entity);
-//        log.info("entity={}", entity);
-//        
-//        return entity;
-//    }
-//    
-//    @Transactional(readOnly = true)
-//    public Post read(Long id) {
-//        log.info("read(id={})", id);
-//        
-//        return postRepository.findById(id).orElseThrow();
-//    }
+    // DB POSTS 테이블에 엔터티를 삽입(insert):
+    public Post create(PostCreateDto dto) {
+        log.info("create(dto={})", dto);
+        
+        // DTO를 Entity로 변환:
+        Post entity = dto.toEntity();
+        log.info("entity={}", entity);
+        
+        postRepository.save(entity);
+        log.info("entity={}", entity);
+        
+        return entity;
+    }
+    
+    @Transactional(readOnly = true)
+    public Post read(Long id) {
+        log.info("read(id={})", id);
+        
+        return postRepository.findById(id).orElseThrow();
+    }
 //    
 //    // DB POSTS 테이블에 엔터티 업데이트:
 //    
