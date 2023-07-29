499a20a5dfea32352047ff537c7bd2ab9a8fb015
kkwh <121307297+kkwh@users.noreply.github.com>
2023-07-30 00:22:01 +0900

    [김우현] Feat: 게시글 기본 CRUD

---
+++ src/main/java/com/example/forest/controller/PostRestController.java
@@ -0,0 +1,48 @@
+package com.example.forest.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.forest.dto.post.ModifyPasswordCheckDto;
+import com.example.forest.model.Post;
+import com.example.forest.service.PostService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/post")
+public class PostRestController {
+    
+	private final PostService postService;
+	
+	@PostMapping("/check-password")
+    public boolean checkPassword(@RequestBody ModifyPasswordCheckDto dto) {
+		log.info("checkPassword(dto={})", dto);
+		
+        String inputPassword = dto.getPassword();
+        Long postId = dto.getPostId();
+
+        Post post = postService.read(postId);
+        log.info("post={}", post);
+
+        return inputPassword.equals(post.getPostPassword());
+    }
+	
+	@DeleteMapping("/{postId}")
+    public ResponseEntity<String> deletePost(@PathVariable long postId) {
+        log.info("deletePost(id={})", postId);
+        
+        postService.delete(postId);
+        
+        return ResponseEntity.ok("success");
+    }
+    
+}
---
+++ src/main/java/com/example/forest/dto/post/PostUpdateDto.java
@@ -0,0 +1,27 @@
+package com.example.forest.dto.post;
+
+
+import com.example.forest.model.Post;
+
+import lombok.Data;
+
+@Data
+public class PostUpdateDto {
+    
+	private Long id;
+    private String postNickname;
+    private String postPassword;
+    private String postTitle;
+    private String postContent;
+    
+    // DTO를 엔터티 객체로 변환해서 리턴하는 메서드:
+    public Post toEntity() {
+        return Post.builder()
+        		.id(id)
+                .postNickname(postNickname)
+                .postPassword(postPassword)
+                .postTitle(postTitle)
+                .postContent(postContent)
+                .build();
+    }
+}
