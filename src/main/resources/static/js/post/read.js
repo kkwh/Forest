bd95c8e7ba6d727daac4ca7cd29cb3f2fc3610a0
kkwh <121307297+kkwh@users.noreply.github.com>
2023-08-17 08:12:16 +0900

    [김우현] Feat: 회원 nickname 클릭 시 정원가기 아코디언 초안

--- src/main/resources/static/js/post/read.js
+++ src/main/resources/static/js/post/read.js
@@ -10,4 +10,20 @@ document.addEventListener("DOMContentLoaded", function() {
             link.classList.add('active-link');
         }
     });
+    
+    
+    // 회원 클릭 시 아코디언, 정원 가기
+    const authors = document.querySelectorAll('.author');
+  authors.forEach((author) => {
+    author.addEventListener('click', function (event) {
+      const accordion = author.querySelector('.author-accordion');
+      if (accordion) {
+        if (accordion.style.display === 'none' || accordion.style.display === '') {
+          accordion.style.display = 'block';
+        } else {
+          accordion.style.display = 'none';
+        }
+      }
+    });
+  });
 });
