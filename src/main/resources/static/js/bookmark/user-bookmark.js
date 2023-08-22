e5b3784d592cc7ad99247b696fe9557d619c6505
Wonjun Seo <93713151+WonjunS@users.noreply.github.com>
2023-08-22 10:54:11 +0900

    [서원준] user-bookmark.js 누락된 코드 추가

--- src/main/resources/static/js/bookmark/user-bookmark.js
+++ src/main/resources/static/js/bookmark/user-bookmark.js
@@ -3,7 +3,56 @@
  */
 document.addEventListener('DOMContentLoaded', () => {
 	
+	/**
+	 * 사용자가 클릭한 게시판을 즐겨찾기 목록에서 삭제하는 메서드
+	 */
+	const deleteBookmark = (e) => {
+		const id = e.target.getAttribute('data-id');
+		
+		const url = `/api/v1/bookmark/delete/${id}`;
+		
+		axios.delete(url)
+			.then((response) => {
+				console.log(response.data);
+				
+				window.location.href='/user/bookmarkList';
+				//loadBookmarkList();
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 	
+	const makeBookmarkList = (data) => {
+		let htmlStr = '';
+		
+		for(let bookmark of data) {
+			htmlStr += `
+				<tr>
+					<td>${bookmark.boardId}</td>
+					<td>
+						<a href="/board/${bookmark.boardId}">
+							<img src="${bookmark.file.uploadPath}" alt="Not found" width="100px" height="auto" />
+						</a>
+					</td>
+					<td>
+						<a href="/board/${bookmark.boardId}" class="link-dark">${bookmark.boardName}</a>
+					</td>
+					<td>${bookmark.category}</td>
+					<td>
+						<button type="button" class="btn btn-warning w-100 bookmarkBtn" data-id="${bookmark.id}"> ☆ </button>
+					</td>
+				</tr>
+			`;
+		}
+		
+		document.querySelector('tbody#bookmark-list').innerHTML = htmlStr;
+		
+		bookmarkBtns = document.querySelectorAll('button.bookmarkBtn');
+		for(let btn of bookmarkBtns) {
+			btn.addEventListener('click', deleteBookmark);
+		}
+	};
 	
 	const loadBookmarkList = async () => {
 		const userId = document.querySelector('input#userId').value;
